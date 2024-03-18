dd127fd32e7b3d452de952c4a7426394bed1156c
Mousa Al-halabi <fadialayyas@gmail.com>
2024-03-18 13:46:01 +0100

    add reward system

--- routes/api.php
+++ routes/api.php
@@ -34,9 +34,9 @@ use App\Http\Controllers\SettingsController;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------
 | API Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
