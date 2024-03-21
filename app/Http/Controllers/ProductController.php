fa263da5519b31b9bc916f3dca210d7d065533b0
Issa Al-Halabi <eesahalabi@gmail.com>
2024-03-21 11:14:42 +0300

    fix

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\ProductResource;
 use App\Http\Resources\RelatedProductResource;
 use App\Models\Product;
 use App\Services\ProductService;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
