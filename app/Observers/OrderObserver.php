f871e0f094e27d86c8bc8337c7626a3b7cd2489b
mousa hlp <mousahlp@gmail.com>
2024-04-22 17:13:00 +0300

    fix

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -15,18 +15,6 @@ class OrderObserver
 {
     use RewardRequests;
 
-    public function created(Order $order): void
-    {
-        $this->rewardPostRequest(RewardRoutes::add_points_to_user, [
-            "user_id" => $order->user_id,
-            "points" => (int) floor($order->total / 1000),
-            "achievement_id" => 5
-        ]);
-        $fcm_token = User::where("id", $order->user_id)->get()->first()->fcm_token;
-        $data = NotificationHelper::getTranslatedData((int) floor($order->total / 1000), "order_created");
-
-        NotificationHelper::sendPushNotification([$fcm_token], $data, NotificationsTypes::Offers);
-    }
     public function updated(Order $order): void
     {
         // if the order was rated 
